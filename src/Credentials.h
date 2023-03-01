a1aff6f7ec14a7ff32f9e93289167b7e294b8398
thecannawiki <98982171+thecannawiki@users.noreply.github.com>
2023-03-01 19:46:27 +0000

    Teplate

---
+++ src/Credentials.h
@@ -0,0 +1,13 @@
+#define SSIDNAME ""
+#define WIFIPASS ""
+#define MQTTURL ""
+#define MQTTUSER ""
+#define MQTTPASS ""
+#define MQTTPORT 8883
+#define MQTTPUBLISHTOPIC ""
+#define MQTTCONTROLTOPIC ""
+#define LED true
+
+// Comment out ONE (1) of these lines to select bme280 (BME) or SCD40 (SCD4) as the connected sensor
+//#define BME
+#define SCD4
